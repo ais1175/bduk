acabb7d8782fd4e2b856696dca45977bf01bf93c
Case <90377400+CaseIRL@users.noreply.github.com>
2025-07-01 19:07:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,7 +18,7 @@
 #                           #                       
 #############################                                         
 #           BDUK            #
-#          V0.2.0           #    
+#          V0.3.0           #    
 #############################
 ]]
 
@@ -26,7 +26,7 @@ fx_version "cerulean"
 games { "gta5", "rdr3" }
 
 name "bduk"
-version "0.2.0"
+version "0.3.0"
 description "BOII Development UI Kit."
 author "boiidevelopment"
 repository "https://github.com/boiidevelopment/bduk"
